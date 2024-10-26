fc48d0ca0562fed0576df230ee75f13c54e22fd6
Croquignolex <geptitalex10@gmail.com>
2024-10-26 09:07:31 +0100

    Add project files

---
+++ croquy-expense-tracker-api-spring-java/src/test/java/com/tracker/croquy/croquyexpensetrackerapispringjava/CroquyExpenseTrackerApiSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.croquy.croquyexpensetrackerapispringjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CroquyExpenseTrackerApiSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
